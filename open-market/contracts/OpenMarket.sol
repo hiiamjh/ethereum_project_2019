46a9063369918ff3e2bc9b9e6c136221c5368282
hiiamjh <hiiamjh@gmail.com>
2019-05-28 18:36:46 +0900

    adding files

--- open-market/contracts/OpenMarket.sol
+++ open-market/contracts/OpenMarket.sol
@@ -18,37 +18,64 @@ contract OpenMarket {
         uint phone;
     }
 
-    struct Payment{ //결제 정보
-        address sellerAddress;
+    struct Payment{ //구매확정 정보
+        address buyerAddress;
+        uint date;
+        bytes32 review;
+    }
+
+    struct Cancel{ //구매취소 정보
+        address buyerAddress;
+        uint date;
+        bytes32 reason;
+    }
+
+    struct doPay{
+        address managerAddress;
+        uint date;
+    }
+
+    struct doCancel{ //환불 정보
+        address managerAddress;
         uint date;
     }
 
     //상품 ID에 해당하는 구매자, 판매자, 결제정보를 각각 Info에 저장
     mapping (uint => Buyer) public buyerInfo;
     mapping (uint => Seller) public sellerInfo;
-    mapping (uint => Payment) public paymentInfo;
-    mapping (address => uint) public balance;
+    mapping (uint => Payment) public decidePaymentInfo;
+    mapping (uint => Cancel) public decideCancelInfo;
+    mapping (uint => doPay) public doPaymentInfo;
+    mapping (uint => doCancel) public doCancelInfo;
 
     address public owner; //컨트랙트 소유자 계정
     address[10] public buyers; //구매자 목록
     address[10] public sellers; //판매자 목록
-    address[10] public payments; //결제 목록
+    address[10] public decidePayments; //구매확정 목록
+    address[10] public decideCancels; //구매취소 목록
+    address[10] public doPayments; //정산목록
+    address[10] public doCancels; //환불목록
 
     event LogBuyProduct(address _buyer, uint _id); //상품 구입 시, 생기는 event
     event LogDeliveryProduct(address _seller, uint _id); //상품 배송 시, 생기는 event
-    event LogPaymentProduct(address _seller, uint _id); //상품 구매확인 시, 생기는 event
+    event LogDecidePayment(address _buyer, uint _id); //상품 구매확정 시, 생기는 event
+    event LogDecideCancel(address _buyer, uint _id); //상품 구매취소 시, 생기는 event
+    event LogPaymentProduct(address _manager, uint _id); //상품 금액정산 시, 생기는 event
+    event LogCancelProduct(address _manager, uint _id); //상품 금액환불 시, 생기는 event
 
     constructor() public {
         owner = msg.sender;
+        //msg.sender는 현재 사용하는 계정으로 컨트랙트의 소유자나 함수를 불러오는 것으로 주소형, default는 첫 번째 계정
     }
 
     //상품 구입시, 금액이 소유자 계정으로 이동
+    //매개변수로 계정, 이름, 나이, 집주소, 우편번호, 핸드폰번호를 이용
     function buyProduct(uint _id, bytes32 _name, uint _age, bytes32 _home, uint _post, uint _phone) public payable {
         require(_id >= 0 && _id <= 9, "the input shoud be included in the _id's ranges");
-        buyers[_id] = msg.sender;
-        buyerInfo[_id] = Buyer(msg.sender, _name, _age, _home, _post, _phone);
+        buyers[_id] = msg.sender; //현재 상품을 구매하는 계정을 구매자배열에 저장하는데, 매물의 id를 인덱스값으로 저장
+        buyerInfo[_id] = Buyer(msg.sender, _name, _age, _home, _post, _phone); //구매자의 정보를 저장
 
-        address(uint160(owner)).transfer(msg.value);
+        address(uint160(owner)).transfer(msg.value); //함수에서 넘겨받은 값을 msg.value로 해서 이더를 전달
         emit LogBuyProduct(msg.sender, _id);
     }
 
@@ -62,14 +89,39 @@ contract OpenMarket {
         emit LogDeliveryProduct(msg.sender, _id);
     }
 
-    //상품 결제 시, 소유자 계정의 금액이 판매자 계정으로 지급
-    function payProduct(uint _id, uint _date) public payable {
+    //구매확정
+    function decidePayProduct(uint _id, uint _date, bytes32 _review) public {
         require(_id >= 0 && _id <= 9, "the input shoud be included in the _id's ranges");
-        payments[_id] = msg.sender;
-        paymentInfo[_id] = Payment(msg.sender, _date);
-        balance[owner] -= msg.value;
-        balance[msg.sender] += msg.value;
+        decidePayments[_id] = msg.sender;
+        decidePaymentInfo[_id] = Payment(msg.sender, _date, _review);
+        //require(msg.sender == address(this), "to tranfer ether from contract to address");
+        emit LogDecidePayment(msg.sender, _id);
+    }
+
+    //금액정산
+    function doPayProduct(uint _id, uint _date) public payable {
+        require(_id >= 0 && _id <= 9, "the input shoud be included in the _id's ranges");
+        doPayments[_id] = msg.sender;
+        doPaymentInfo[_id] = doPay(msg.sender, _date);
+        address(uint160(sellers[_id])).transfer(msg.value);
         emit LogPaymentProduct(msg.sender, _id);
+    }
+
+    //구매취소
+    function decideCancelProduct(uint _id, uint _date, bytes32 _reason) public {
+        require(_id >= 0 && _id <= 9, "the input shoud be included in the _id's ranges");
+        decideCancels[_id] = msg.sender;
+        decideCancelInfo[_id] = Cancel(msg.sender, _date, _reason);
+        emit LogDecideCancel(msg.sender, _id);
+    }
+
+    //금액환불
+    function doCancelProduct(uint _id, uint _date) public payable {
+        require(_id >= 0 && _id <= 9, "the input shoud be included in the _id's ranges");
+        doCancels[_id] = msg.sender;
+        doCancelInfo[_id] = doCancel(msg.sender, _date);
+        address(uint160(buyers[_id])).transfer(msg.value);
+        emit LogCancelProduct(msg.sender, _id);
     }
 
     //구매자 정보
@@ -94,15 +146,47 @@ contract OpenMarket {
         return sellers;
     }
 
-    //결제 정보
-    function getPaymentInfo(uint _id) public view returns (address, uint) {
-        Payment memory payment = paymentInfo[_id];
-        return (payment.sellerAddress, payment.date);
+    //구매확정 정보
+    function getPaymentInfo(uint _id) public view returns (address, uint, bytes32) {
+        Payment memory payment = decidePaymentInfo[_id];
+        return (payment.buyerAddress, payment.date, payment.review);
     }
 
-    //모든 결제 정보
+    //모든 구매확정 정보
     function getAllPayments() public view returns (address[10] memory) {
-        return payments;
+        return decidePayments;
     }
 
+    //구매취소 정보
+    function getCancelInfo(uint _id) public view returns (address, uint, bytes32) {
+        Cancel memory cancel = decideCancelInfo[_id];
+        return (cancel.buyerAddress, cancel.date, cancel.reason);
+    }
+
+    //모든 구매취소 정보
+    function getAllCancels() public view returns (address[10] memory) {
+        return decideCancels;
+    }
+
+    //정산정보
+    function getdoPaymentInfo(uint _id) public view returns (address, uint) {
+        doPay memory dopay = doPaymentInfo[_id];
+        return (dopay.managerAddress, dopay.date);
+    }
+
+    //모든 정산 정보
+    function getAlldoPayments() public view returns (address[10] memory) {
+        return doPayments;
+    }
+    
+    //환불정보
+    function getdoCancelInfo(uint _id) public view returns (address, uint) {
+        doCancel memory docancel = doCancelInfo[_id];
+        return (docancel.managerAddress, docancel.date);
+    }
+
+    //모든 환불 정보
+    function getAlldoCancels() public view returns (address[10] memory) {
+        return doCancels;
+    }
 }
--- open-market/src/js/app.js
+++ open-market/src/js/app.js
@@ -63,7 +63,7 @@ App = {
         console.log(error);
       }
 
-      var account = accounts[0];
+      var account = accounts[0]; //현재 선택된 계정
 
       //인스턴스화 시킨 컨트랙트의 함수를 이용
       App.contracts.OpenMarket.deployed().then(function(instance) {
@@ -73,6 +73,7 @@ App = {
         var homeUtf8Encoded = utf8.encode(home);
         
         //컨트랙트 내 buyProduct함수에 인자값 전달
+        //from에서 함수를 불러오고, price값을 컨트랙트에서 지정해준 owner함수로 이더를 전송
         return instance.buyProduct(id, web3.toHex(nameUtf8Encoded), age, web3.toHex(homeUtf8Encoded), post, phone, { from: account, value: price });
       }).then(function() {
         //함수 실행이 끝나면, html의 modal 값을 비우고, buyModal을 감춤
@@ -125,24 +126,96 @@ App = {
     });
   },
 
-  //금액정산
-  payProduct: function() {	
+  //구매확정
+  decidePayProduct: function() {	
     var id = $('#id').val();
-    var price = $('#price').val();
-    var date = $('#date').val();
+    var paydate = $('#paydate').val();
+    var review = $('#review').val();
 
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
-
-      var account = accounts[0]; //현재 선택된 계정
+      //var secondAddress = 0xDc27C2b26EDbfb7Eb223589D4997dDA997DA8D1e;
       App.contracts.OpenMarket.deployed().then(function(instance) {
-        return instance.payProduct(id, date, { from: account, value: price });
+        var reviewUtf8Encoded = utf8.encode(review);
+        return instance.decidePayProduct(id, paydate, web3.toHex(reviewUtf8Encoded));
       }).then(function() {
         id++;
-        $('#date').val('');
-        $('#payModal').modal('hide');
+        $('#paydate').val('');
+        $('#review').val('');
+        $('#decisionModal').modal('hide');
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  //구매취소
+  decideCancelProduct: function() {	
+    var id = $('#id').val();
+    var canceldate = $('#canceldate').val();
+    var reason = $('#reason').val();
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      //var secondAddress = 0xDc27C2b26EDbfb7Eb223589D4997dDA997DA8D1e;
+      App.contracts.OpenMarket.deployed().then(function(instance) {
+        var reasonUtf8Encoded = utf8.encode(reason);
+        return instance.decideCancelProduct(id, canceldate, web3.toHex(reasonUtf8Encoded));
+      }).then(function() {
+        id++;
+        $('#canceldate').val('');
+        $('#reason').val('');
+        $('#decisionModal').modal('hide');
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  //금액정산
+  doPayProduct: function() {	
+    var id = $('#id').val();
+    var price = $('#price').val();
+    var pdate = $('#pdate').val();
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      var account = accounts[0];
+      //var secondAddress = 0xDc27C2b26EDbfb7Eb223589D4997dDA997DA8D1e;
+      App.contracts.OpenMarket.deployed().then(function(instance) {
+        return instance.doPayProduct(id, pdate, {from: account, value: price});
+      }).then(function() {
+        id++;
+        $('#doPayModal').modal('hide');
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  //금액환불
+  doCancelProduct: function() {	
+    var id = $('#id').val();
+    var price = $('#price').val();
+    var cdate = $('#cdate').val();
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      var account = accounts[0];
+      //var secondAddress = 0xDc27C2b26EDbfb7Eb223589D4997dDA997DA8D1e;
+      App.contracts.OpenMarket.deployed().then(function(instance) {
+        return instance.doCancelProduct(id, cdate, {from: account, value: price});
+      }).then(function() {
+        id++;
+        $('#doCancelModal').modal('hide');
       }).catch(function(err) {
         console.log(err.message);
       });
@@ -172,6 +245,7 @@ App = {
         if (!error) {
           $('#sellerevents').append('<p>' + event.args._buyer + ' 구매자가 ' + event.args._id + ' 번 상품을 구입했습니다.' + '</p>');
           $('#buyerevents').append('<p>' + event.args._id + ' 번 상품을 구입했습니다.' + '</p>');
+          $('#managerevents').append('<p>' + event.args._id + ' 번 상품이 ' + event.args._buyer + ' 구매자로부터 구입되었습니다.' + '</p>');
 
         } else {
           console.error(error);
@@ -185,6 +259,7 @@ App = {
       if (!error) {
         $('#buyerevents').append('<p><span style="color:Blue">' + event.args._seller + ' 판매자가 ' + event.args._id + ' 번 상품을 배송했습니다.' + '</span></p>');
         $('#sellerevents').append('<p><span style="color:Blue">' + event.args._id + ' 번 상품을 배송했습니다.' + '</span></p>');
+        $('#managerevents').append('<p>' + event.args._id + ' 번 상품이 ' + event.args._seller + ' 판매자로부터 배송되었습니다.' + '</p>');
 
       } else {
         console.error(error);
@@ -194,15 +269,54 @@ App = {
   })
 
   App.contracts.OpenMarket.deployed().then(function(instance) {
-    instance.LogPaymentProuct({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+    instance.LogDecidePayment({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
       if (!error) {
-        $('#buyerevents').append('<p><span style="color:Gray">' + event.args._seller + ' 판매자에게 ' + event.args._id + ' 번 상품이 정산되었습니다.' + '</span></p>');
-        $('#sellerevents').append('<p><span style="color:Gray">' + event.args._id + ' 번 상품금액이 정산되었습니다.' + '</span></p>');
+        $('#buyerevents').append('<p><span style="color:Gray">' + event.args._id + ' 번 상품의 구매를 확정하였습니다.' + '</span></p>');
+        $('#sellerevents').append('<p><span style="color:Gray">' + event.args._buyer + ' 구매자가 ' + event.args._id + ' 번 상품의 구매를 확정하였습니다.' + '</span></p>');
+        $('#managerevents').append('<p><span style="color:Gray">' + event.args._buyer + ' 구매자가 ' + event.args._id + ' 번 상품의 구매를 확정하였습니다.' + '</span></p>');
+      } else {
+        console.error(error);
+      }
+      App.loadProducts5();
+    })
+  })
 
+  App.contracts.OpenMarket.deployed().then(function(instance) {
+    instance.LogDecideCancel({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+      if (!error) {
+        $('#buyerevents').append('<p><span style="color:Gray">' + event.args._id + ' 번 상품의 구매를 취소하였습니다.' + '</span></p>');
+        $('#sellerevents').append('<p><span style="color:Gray">' + event.args._buyer + ' 구매자가 ' + event.args._id + ' 번 상품의 구매를 취소하였습니다.' + '</span></p>');
+        $('#managerevents').append('<p><span style="color:Gray">' + event.args._buyer + ' 구매자가 ' + event.args._id + ' 번 상품의 구매를 취소하였습니다.' + '</span></p>');
+      } else {
+        console.error(error);
+      }
+      App.loadProducts6();
+    })
+  })
+
+  App.contracts.OpenMarket.deployed().then(function(instance) {
+    instance.LogPaymentProduct({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+      if (!error) {
+        $('#buyerevents').append('<p><span style="color:Gray">' + ' 판매자에게 ' + event.args._id + ' 번 상품이 정산되었습니다.' + '</span></p>');
+        $('#sellerevents').append('<p><span style="color:Gray">' + event.args._id + ' 번 상품금액이 정산되었습니다.' + '</span></p>');
+        $('#managerevents').append('<p><span style="color:Gray">' + event.args._manager + ' 운영자가 ' + event.args._id + ' 번 상품을 정산하었습니다.' + '</span></p>');
       } else {
         console.error(error);
       }
       App.loadProducts3();
+    })
+  })
+
+  App.contracts.OpenMarket.deployed().then(function(instance) {
+    instance.LogCancelProduct({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+      if (!error) {
+        $('#buyerevents').append('<p><span style="color:Gray">' + event.args._id + ' 번 상품이 환불되었습니다.' + '</span></p>');
+        $('#sellerevents').append('<p><span style="color:Gray">' + ' 구매자에게' + event.args._id + ' 번 상품금액이 환불되었습니다.' + '</span></p>');
+        $('#managerevents').append('<p><span style="color:Gray">' + event.args._manager + ' 구매자가 ' + event.args._id + ' 번 상품을 환불하었습니다.' + '</span></p>');
+      } else {
+        console.error(error);
+      }
+      App.loadProducts4();
     })
   })
   },
@@ -230,13 +344,66 @@ App = {
   //정산 후, 해당 내역이 업데이트 된 상품목록을 로드시킴
   loadProducts3: function() {
     App.contracts.OpenMarket.deployed().then(function(instance) {
-      return instance.getAllPayments.call();
-    }).then(function(payments) {
-      for (i = 0; i < payments.length; i++) {
-        if (payments[i] !== '0x0000000000000000000000000000000000000000') {
+      return instance.getAlldoPayments.call();
+    }).then(function(doPayments) {
+      for (i = 0; i < doPayments.length; i++) {
+        if (doPayments[i] !== '0x0000000000000000000000000000000000000000') {
 
-          $('.panel-realEstate').eq(i).find('.btn-pay').text('정산완료').attr('disabled', true);
-          $('.panel-realEstate').eq(i).find('.seller-payInfo').removeAttr('style');             
+          $('.panel-realEstate').eq(i).find('.btn-dopay').text('정산완료').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-docancel').text('환불불가').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-decidedPayInfo').removeAttr('style');
+          $('.panel-realEstate').eq(i).find('.dopaymsg').removeAttr('style');           
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
+  },
+
+  //환불 후, 해당 내역이 업데이트 된 상품목록을 로드시킴
+  loadProducts4: function() {
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getAlldoCancels.call();
+    }).then(function(doCancels) {
+      for (i = 0; i < doCancels.length; i++) {
+        if (doCancels[i] !== '0x0000000000000000000000000000000000000000') {
+
+          $('.panel-realEstate').eq(i).find('.btn-docancel').text('환불완료').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-dopay').text('정산불가').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-decidedCancelInfo').removeAttr('style'); 
+          $('.panel-realEstate').eq(i).find('.docancelmsg').removeAttr('style');            
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
+  },
+
+  loadProducts5: function() {
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getAllPayments.call();
+    }).then(function(decidePayments) {
+      for (i = 0; i < decidePayments.length; i++) {
+        if (decidePayments[i] !== '0x0000000000000000000000000000000000000000') {
+
+          $('.panel-realEstate').eq(i).find('.btn-decision').text('구매확정완료').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-docancel').text('환불불가').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
+  },
+
+  loadProducts6: function() {
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getAllCancels.call();
+    }).then(function(decideCancels) {
+      for (i = 0; i < decideCancels.length; i++) {
+        if (decideCancels[i] !== '0x0000000000000000000000000000000000000000') {
+
+          $('.panel-realEstate').eq(i).find('.btn-decision').text('구매취소완료').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-dopay').text('정산불가').attr('disabled', true);          
         }
       }
     }).catch(function(err) {
@@ -277,6 +444,17 @@ $(function() {
       $(e.currentTarget).find('#buyerPhone').text(buyerInfo[5]);
     }).catch(function(err) {
       console.log(err.message);
+    }),
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getPaymentInfo.call(id);
+    }).then(function(decidePaymentInfo) {
+      //$(e.currentTarget).find('#payAddress').text(paymentInfo[0]);
+      $(e.currentTarget).find('#frompaybuyer').text(decidePaymentInfo[0]);
+      $(e.currentTarget).find('#frompaydate').text(decidePaymentInfo[1]);
+      $(e.currentTarget).find('#fromreview').text(web3.toUtf8(decidePaymentInfo[2]));
+    }).catch(function(err) {
+      console.log(err.message);
     })
   });
 
@@ -309,24 +487,100 @@ $(function() {
     })
   });
 
-  $('#payModal').on('show.bs.modal', function(e) {
+  $('#decisionModal').on('show.bs.modal', function(e) {
     var id = $(e.relatedTarget).parent().find('.id').text();
     var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
 
     $(e.currentTarget).find('#id').val(id);
     $(e.currentTarget).find('#price').val(price);
+
   });
 
-  $('#payInfoModal').on('show.bs.modal', function(e) {
+  $('#doPayModal').on('show.bs.modal', function(e) {
+    var id = $(e.relatedTarget).parent().find('.id').text();
+    var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
+
+    $(e.currentTarget).find('#id').val(id);
+    $(e.currentTarget).find('#price').val(price);
 
     App.contracts.OpenMarket.deployed().then(function(instance) {
       return instance.getPaymentInfo.call(id);
-    }).then(function(paymentInfo) {
-      $(e.currentTarget).find('#payAddress').text(buyerInfo[0]);
-      $(e.currentTarget).find('#payDate').text(paymentInfo[1]);
+    }).then(function(decidePaymentInfo) {
+      //$(e.currentTarget).find('#payAddress').text(paymentInfo[0]);
+      $(e.currentTarget).find('#frompaybuyer').text(decidePaymentInfo[0]);
+      $(e.currentTarget).find('#frompaydate').text(decidePaymentInfo[1]);
+      $(e.currentTarget).find('#fromreview').text(web3.toUtf8(decidePaymentInfo[2]));
     }).catch(function(err) {
       console.log(err.message);
     })
+  });
+
+  $('#decidedPayInfoModal').on('show.bs.modal', function(e) {
+
+    var id = $(e.relatedTarget).parent().find('.id').text();
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getPaymentInfo.call(id);
+    }).then(function(decidePaymentInfo) {
+      //$(e.currentTarget).find('#payAddress').text(paymentInfo[0]);
+      $(e.currentTarget).find('#frompaybuyer').text(decidePaymentInfo[0]);
+      $(e.currentTarget).find('#frompaydate').text(decidePaymentInfo[1]);
+      $(e.currentTarget).find('#fromreview').text(web3.toUtf8(decidePaymentInfo[2]));
+    }).catch(function(err) {
+      console.log(err.message);
+    }),
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getdoPaymentInfo.call(id);
+    }).then(function(doPaymentInfo) {
+      $(e.currentTarget).find('#frommanager').text(doPaymentInfo[0]);
+      $(e.currentTarget).find('#frompdate').text(doPaymentInfo[1]);
+    })
+  });
+  });
+
+  $('#doCancelModal').on('show.bs.modal', function(e) {
+    var id = $(e.relatedTarget).parent().find('.id').text();
+    var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
+
+    $(e.currentTarget).find('#id').val(id);
+    $(e.currentTarget).find('#price').val(price);
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getCancelInfo.call(id);
+    }).then(function(decideCancelInfo) {
+      //$(e.currentTarget).find('#payAddress').text(paymentInfo[0]);
+      $(e.currentTarget).find('#frompaybuyer').text(decideCancelInfo[0]);
+      $(e.currentTarget).find('#fromcanceldate').text(decideCancelInfo[1]);
+      $(e.currentTarget).find('#fromreason').text(web3.toUtf8(decideCancelInfo[2]));
+    }).catch(function(err) {
+      console.log(err.message);
+    })
+
+  });
+
+  $('#decidedCancelInfoModal').on('show.bs.modal', function(e) {
+
+    var id = $(e.relatedTarget).parent().find('.id').text();
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getCancelInfo.call(id);
+    }).then(function(decideCancelInfo) {
+      //$(e.currentTarget).find('#payAddress').text(paymentInfo[0]);
+      $(e.currentTarget).find('#fromcancelbuyer').text(decideCancelInfo[0]);
+      $(e.currentTarget).find('#fromcanceldate').text(decideCancelInfo[1]);
+      $(e.currentTarget).find('#fromreason').text(web3.toUtf8(decideCancelInfo[2]));
+    }).catch(function(err) {
+      console.log(err.message);
+    }),
+
+    App.contracts.OpenMarket.deployed().then(function(instance) {
+      return instance.getdoCancelInfo.call(id);
+    }).then(function(doCancelInfo) {
+      $(e.currentTarget).find('#frommanager').text(doCancelInfo[0]);
+      $(e.currentTarget).find('#fromcdate').text(doCancelInfo[1]);
+    }).catch(function(err) {
+    console.log(err.message);
   });
 
 });
